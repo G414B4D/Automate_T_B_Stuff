d5b5152c104cec28a8f509e9f435cc05b5a939c5
G414B4D <108630779+G414B4D@users.noreply.github.com>
2025-07-07 22:10:16 -0700

    Things from the start of the book uploaded

---
+++ Automate_T_B_Stuff/for.py
@@ -0,0 +1,18 @@
+print('My name is')
+for i in range (5):
+    print('Jimmy five times')
+
+print('\n')
+
+total = 50
+for num in range(101):
+    total = total + num
+print(total)
+
+print('\n')
+
+i = 0
+while i < 5:
+    print ('Almost Jimmy 5 Times ' + str(i))
+    i = i + 1
+
